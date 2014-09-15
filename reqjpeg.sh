aff358a50247410ee237933d76f7c21e9425c659
Ben Ransford <ben@ransford.org>
2014-09-15 15:39:53 -0700

    record recvfile stderr to fox.log

--- reqjpeg.sh
+++ reqjpeg.sh
@@ -4,7 +4,7 @@ HOST=${1:-localhost}
 
 test -f ./libsap/examples/recvfile/recvfile || make -C libsap || exit 1
 
-./libsap/examples/recvfile/recvfile htdocs/fox.jpg fox-tmp.jpg &
+./libsap/examples/recvfile/recvfile htdocs/fox.jpg fox-tmp.jpg 2>fox.log &
 
 curl -v -O -H 'X-SAP-Approx: image/jpeg, image/tiff' \
 	"http://${HOST}:8099/fox.jpg"
