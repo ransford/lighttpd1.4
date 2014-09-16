725675413c215bb74b93898ccaace48cbc4c0c6f
Ben Ransford <ben@ransford.org>
2014-09-16 11:25:45 -0700

    reqjpeg: do not hardcode fox

--- reqjpeg.sh
+++ reqjpeg.sh
@@ -1,12 +1,13 @@
 #!/bin/sh
 
 HOST=${1:-localhost}
+shift
+JPEG=${1:-fox}
 
 test -f ./libsap/examples/recvfile/recvfile || make -C libsap || exit 1
 
-./libsap/examples/recvfile/recvfile htdocs/fox.jpg fox-tmp.jpg 2>fox.log &
+./libsap/examples/recvfile/recvfile "htdocs/${JPEG}.jpg" "${JPEG}-tmp.jpg" 2>"${JPEG}.log" &
 
-curl -v -O -H 'X-SAP-Approx: image/jpeg, image/tiff' \
-	"http://${HOST}:8099/fox.jpg"
+curl -v -O -H 'X-SAP-Approx: image/jpeg, image/tiff' "http://${HOST}:8099/${JPEG}.jpg"
 
-sha1sum htdocs/fox.jpg fox-tmp.jpg
+sha1sum "htdocs/${JPEG}.jpg" "${JPEG}-tmp.jpg"
