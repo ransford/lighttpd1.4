7e5deb4670d6162380b7001fa47ca2a6ad6c1741
Ben Ransford <ben@ransford.org>
2014-09-16 21:17:31 -0700

    report curl timings

--- reqjpeg.sh
+++ reqjpeg.sh
@@ -6,8 +6,12 @@ JPEG=${1:-fox}
 
 test -f ./libsap/examples/recvfile/recvfile || make -C libsap || exit 1
 
-./libsap/examples/recvfile/recvfile "htdocs/${JPEG}.jpg" "${JPEG}-tmp.jpg" 2>"${JPEG}.log" &
+./libsap/examples/recvfile/recvfile \
+	"htdocs/${JPEG}.jpg" "${JPEG}-tmp.jpg" \
+	2>"${JPEG}.log" &
 
-curl -v -O -H 'X-SAP-Approx: image/jpeg, image/tiff' "http://${HOST}:8099/${JPEG}.jpg"
+curl -v -w '@curlreport.txt' -O \
+	-H 'X-SAP-Approx: image/jpeg, image/tiff' \
+	"http://${HOST}:8099/${JPEG}.jpg"
 
 sha1sum "htdocs/${JPEG}.jpg" "${JPEG}-tmp.jpg"
